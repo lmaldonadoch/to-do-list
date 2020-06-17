b4eb1205cea331aacc873079a90e1f93ce00f2c6
idgm5 <idgm@pm.me>
2020-06-17 15:17:59 -0500

    WIP todoJS

---
+++ src/todo.js
@@ -0,0 +1,15 @@
+
+
+export default class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.check = false;
+  }
+
+  addTodo() {
+    
+  }
+}
